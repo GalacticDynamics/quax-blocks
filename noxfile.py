6f2ebfe47a4eace6bd75c190eb13fda84816ff6d
nstarman <nstarman@users.noreply.github.com>
2025-11-23 17:12:36 -0500

    deps: update for nox
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,9 @@ import shutil
 from pathlib import Path
 
 import nox
+from nox_uv import session
 
+nox.needs_version = ">=2024.3.2"
 nox.options.sessions = [
     # Linting
     "lint",
@@ -12,8 +14,9 @@ nox.options.sessions = [
     "precommit",
     # Testing
     "tests",
-    # Building
-    "build",
+    # Documentation
+    "docs",
+    "build_api_docs",
 ]
 nox.options.default_venv_backend = "uv"
 
@@ -23,71 +26,45 @@ DIR = Path(__file__).parent.resolve()
 # Linting
 
 
-@nox.session(venv_backend="uv")
-def lint(session: nox.Session, /) -> None:
+@session(uv_groups=["lint"], reuse_venv=True)
+def lint(s: nox.Session, /) -> None:
     """Run the linter."""
-    precommit(session)  # reuse pre-commit session
-    pylint(session)  # reuse pylint session
+    precommit(s)  # reuse pre-commit session
+    pylint(s)  # reuse pylint session
 
 
-@nox.session(venv_backend="uv")
-def precommit(session: nox.Session, /) -> None:
+@session(venv_backend="uv", reuse_venv=True)
+def precommit(s: nox.Session, /) -> None:
     """Run pre-commit."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--group=lint",
-        f"--python={session.virtualenv.location}",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
-    session.run("pre-commit", "run", "--all-files", *session.posargs)
+    s.run("pre-commit", "run", "--all-files", *s.posargs)
 
 
-@nox.session(venv_backend="uv")
-def pylint(session: nox.Session, /) -> None:
+@session(uv_groups=["lint"], reuse_venv=True)
+def pylint(s: nox.Session, /) -> None:
     """Run PyLint."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--group=lint",
-        f"--python={session.virtualenv.location}",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
-    session.run("pylint", "unxt", *session.posargs)
+    s.install(".", "pylint")
+    s.run("pylint", "quaxed", *s.posargs)
 
 
 # =============================================================================
 # Testing
 
 
-@nox.session(venv_backend="uv")
-def tests(session: nox.Session, /) -> None:
+@session(uv_groups=["test"], reuse_venv=True)
+def tests(s: nox.Session, /) -> None:
     """Run the unit and regular tests."""
-    session.run_install(
-        "uv",
-        "sync",
-        "--group=test",
-        f"--python={session.virtualenv.location}",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
-    session.run("pytest", *session.posargs)
+    s.run("pytest", *s.posargs)
 
 
 # =============================================================================
+# Build
 
 
-@nox.session(venv_backend="uv")
-def build(session: nox.Session, /) -> None:
+@session(reuse_venv=True)
+def build(s: nox.Session, /) -> None:
     """Build an SDist and wheel."""
     build_path = DIR.joinpath("build")
     if build_path.exists():
         shutil.rmtree(build_path)
 
-    session.run_install(
-        "uv",
-        "sync",
-        "--group=build",
-        f"--python={session.virtualenv.location}",
-        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
-    )
-    session.run("build")
+    s.run("uv", "build")
